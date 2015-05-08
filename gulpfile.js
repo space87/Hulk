77a39554ab7eabb68ff06c7bf3eb8f996a7f21ca
Jason Staerck <jasonstaerck@gmail.com>
2015-05-08 18:19:53 +0100

    updated framework to v2 and took out flex and using a single base

--- gulpfile.js
+++ gulpfile.js
@@ -2,38 +2,57 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    sassy = require('gulp-sass'),
+    notify = require('gulp-notify'),
     rename = require('gulp-rename');
 
 
+    
+gulp.task('testCommand', function() {
+    return gulp.src('*.html')
+    .pipe(notify({ message: 'test task complete' }));
+});
 
-gulp.task('express', function() {
-  var express = require('express');
-  var app = express();
-  app.use(express.static(__dirname));
-  app.listen(4000);
+gulp.task('sassLint', function() {
+    gulp.src('sass/page.scss')
+    .pipe(scssLint({
+    'reporterOutput': 'scssReport.xml'
+}))
+    .pipe(notify({ message: 'sass linting pass its good to go' }))
 });
 
 
-gulp.task('styles', function() {
-  return gulp.src('src/sass/**/*.scss')
-    .pipe(sassy({ style: 'expanded' }))
+gulp.task('grid', function() {
+  return sass('grid/main.scss')
     .on('error', function (err) {
       console.error('Error!', err.message);
     })
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(autoprefixer('last 2 version', 'ie 8', 'ie 9'))
     .pipe(gulp.dest('build/css'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest('build/css'))
+    .pipe(notify({ message: 'grid compling complete' }));
+});
+
+gulp.task('page', function() {
+  return sass('sass/page.scss')
+    .on('error', function (err) {
+      console.error('Error!', err.message);
+    })
+    .pipe(autoprefixer('last 2 version', 'ie 8', 'ie 9'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('build/css'))
+  .pipe(notify({ message: 'grid compling complete' }));
 });
 
 
 
 gulp.task('watch', function() {
-  gulp.watch('src/sass/**/*.scss', ['styles']);
+  gulp.watch('sass/**/*.scss', ['page']);
+  gulp.watch('grid/**/*.scss', ['grid']);
 });
 
-gulp.task('default', ['express', 'watch'], function() {
 
-});
+gulp.task('default', ['testCommand', 'watch']);
